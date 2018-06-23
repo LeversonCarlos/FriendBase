f4fa90bf641598f8b813245733bb3b35b671aaf0
Leverson Carlos <lcjohnny@hotmail.com>
2018-06-23 10:35:16 -0300

    Icon resource

--- FriendData.Sqlite/Sqlite.cs
+++ FriendData.Sqlite/Sqlite.cs
@@ -6,5 +6,34 @@ namespace FS.Data
 {
    public class Sqlite : Common.Data
    {
+
+      #region NEW
+      public Sqlite()
+      {
+         this.EngineType += Base_EngineType;
+         // this.GetConnectionStringParams += Base_GetConnectionStringParams;
+         //this.GetConnectionStringSteps += Base_GetConnectionStringSteps;
+         //this.InitializeConnection += Base_InitializeConnection;
+         //this.InitializeAdapter += Base_InitializeAdapter;
+         //this.InitializeCommand += Base_InitializeCommand;
+         //this.InitializeCommandBuilder += Base_InitializeCommandBuilder;
+         //this.InitializeSchema += Base_InitializeSchema;
+         //this.InitializeScript += Base_InitializeScript;
+      }
+      #endregion
+
+      #region EVENTS
+
+      #region Base_EngineType
+      private void Base_EngineType(ref FS.Data.Common.Engine.Type Value)
+      {
+         Value.Key = "sqlite";
+         Value.Description = "SQLite";
+         Value.Icon = Resources.Icon;
+      }
+      #endregion
+
+      #endregion
+
    }
 }
